78f05d874119040e955d8946cfea09a0640ce3df
NikitDudinov <dudinovnik@yandex.ru>
2024-06-27 15:00:28 +0300

    create tetris

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -3,19 +3,38 @@
 namespace App\Controller;
 
 use App\Repository\PlayerRepository;
+use App\Service\PlayerService;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PlayerController extends AbstractController
 {
-    public function __construct(private readonly PlayerRepository $playerRepository)
+    public function __construct(
+        private readonly PlayerService $playerService,
+        private readonly UserService   $userService,
+    )
     {
     }
 
-    public function index(): Response
+    public function updateStatistics(Request $request): Response
     {
-        $player = $this->playerRepository->find(1);
-        var_dump($player);
-        return $this->render('index.html.twig');
+        $securityUser = $this->getUser();
+        if ($securityUser === null) {
+            return $this->json([], Response::HTTP_UNAUTHORIZED);
+        }
+        $user = $this->userService->findUser($securityUser->getId());
+        $player_id = $user->getPlayerId();
+
+        $statistics = json_decode($request->getContent(), true);
+        if ($statistics !== null) {
+            $this->playerService->updateStatistics(
+                $player_id,
+                $statistics['score'] ?? null,
+                $statistics['isWon'] ?? null,
+            );
+        }
+        return new Response("Hello");
     }
 }
