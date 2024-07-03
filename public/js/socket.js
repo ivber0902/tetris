319ee533165723645f73e0e63a8c10437e292d89
Dima Belyaev <dima.dmitry663@gmail.com>
2024-07-03 16:42:04 +0300

    continue socket

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,10 +1,17 @@
 const connection = new WebSocket("ws://localhost:9000");
 let a = 1;
 // если соединение успешно установлено
-connection.onopen = (event) => {  
 
+connection.onopen = (event) => {  
     console.log("Hello METANIT.COM");
-    
+    let profileNicknameElement = document.querySelector('.profile__nickname');
+    if (profileNicknameElement) {
+        let check = profileNicknameElement.innerHTML;
+        console.log('Sending nickname:', check);
+        connection.send(JSON.stringify({ type: 'newUser', name: check }));
+    } else {
+        console.error('Element with class .profile__nickname not found.');
+    }
 };
 // если возникла ошибка
 connection.onerror = (error) => {
@@ -17,11 +24,6 @@ connection.onclose = (event) => {
 // получаем ответ сервера
 connection.onmessage = (event) =>{ 
     console.log("Server response:", event.data);
-};
-connection.onmessage = (event) =>{ 
     document.querySelector('.players__count').innerHTML = event.data;
     let count = event.data;
-    connection.send("her");
 };
-
-
