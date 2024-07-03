2c298f122334b2d04000d17d6124442df55ad157
YelKar <karamyshev_e@1060.ru>
2024-07-03 18:11:51 +0300

    Раелизовал создание и добавление игрока в лобби (Исправлена идейная ошибка и моя шиза)

--- WebSocket/lobby.go
+++ WebSocket/lobby.go
@@ -2,15 +2,15 @@ package main
 
 type Lobby struct {
 	ID       int32   `bson:"_id"`
-	Players  []int32 `bson:"players" json:"players"`
+	Players  []int32 `bson:"players" json:"players,omitempty"`
 	Settings struct {
-		Music      string `bson:"music" json:"music"`
-		Background string `bson:"background" json:"background"`
-		Difficulty int8   `bson:"difficulty" json:"difficulty"`
+		Music      string `bson:"music" json:"music,omitempty"`
+		Background string `bson:"background" json:"background,omitempty"`
+		Difficulty int8   `bson:"difficulty" json:"difficulty,omitempty"`
 
 		PlayField struct {
-			Width  int8 `bson:"width" json:"width"`
-			Height int8 `bson:"height" json:"height"`
-		} `bson:"play_field" json:"play_field"`
-	} `bson:"settings"`
+			Width  int8 `bson:"width" json:"width,omitempty"`
+			Height int8 `bson:"height" json:"height,omitempty"`
+		} `bson:"play_field" json:"play_field,omitempty"`
+	} `bson:"settings,omitempty"`
 }
--- WebSocket/lobby_handlers.go
+++ WebSocket/lobby_handlers.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
-	"os"
 )
-
-type ConnectionJSONRequest struct {
-	LobbyID  int32 `json:"lobby_id"`
-	PlayerID int32 `json:"player_id"`
-}
 
 func ConnectToLobbyHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -27,73 +16,51 @@ func ConnectToLobbyHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}()
 
-	var request ConnectionJSONRequest
+	var request UpdateJSONRequest
 
 	for {
-		err := conn.ReadJSON(&request)
+		err = conn.ReadJSON(&request)
 		if err != nil {
-			log.Println("JSON reading error", err)
-		} else {
-			err = ConnectToLobby(request)
-			log.Println("Connect to lobby error:", err)
+			log.Println("JSON reading error during connection:", err)
+		} else if request.Type == ConnectRequestType {
+			err = ConnectToLobby(request.Connection)
+			if err != nil {
+				log.Println("Connect to lobby error:", err)
+			} else {
+				break
+			}
+		}
+		err := conn.WriteJSON(
+			ConnectionJSONResponse{
+				StatusCode: 400,
+				Message:    "Wrong JSON format",
+			},
+		)
+		if err != nil {
+			log.Println("JSON writing error during connection:", err)
+			return
 		}
 	}
-}
 
-func ConnectToLobby(request ConnectionJSONRequest) error {
-	loadEnv()
-	log.Println("Connecting to lobby", request.LobbyID)
-
-	// Connect to database
-	dbURI := os.Getenv("DATABASE_URI")
-	opt := options.Client().ApplyURI(dbURI)
-	client, err := mongo.Connect(context.TODO(), opt)
-	if err != nil {
-		panic(err)
-	}
-	lobbyTable := client.Database("tetris").Collection("lobby")
-
-	log.Printf("lobby id: %v player id: %v\n", request.LobbyID, request.PlayerID)
-	if request.LobbyID == 0 {
-		request.LobbyID = request.PlayerID
-	}
-	log.Printf("after test => lobby id: %v player id: %v\n", request.LobbyID, request.PlayerID)
-	var lobby Lobby
-	err = lobbyTable.FindOne(context.TODO(), bson.D{{"_id", request.LobbyID}}).Decode(&lobby)
-	if errors.Is(err, mongo.ErrNoDocuments) {
-		err = CreateLobby(request, lobbyTable)
-		return err
-	} else if err != nil {
-		return err
-	}
-	for _, playerID := range lobby.Players {
-		log.Println(playerID)
-		if playerID == request.PlayerID {
-			return nil
+	for {
+		err = conn.ReadJSON(&request)
+		if err != nil {
+			log.Println("JSON reading error:", err)
+			err := conn.WriteJSON(
+				ConnectionJSONResponse{
+					StatusCode: 400,
+					Message:    "Wrong JSON format",
+				},
+			)
+			if err != nil {
+				log.Println("JSON writing error during connection:", err)
+				return
+			}
+		} else if request.Type == UpdateRequestType {
+			err := UpdateLobby(request)
+			if err != nil {
+				log.Println("Update lobby error:", err)
+			}
 		}
 	}
-	lobby.Players = append(lobby.Players, request.PlayerID)
-	_, err = lobbyTable.UpdateOne(
-		context.TODO(),
-		bson.D{{"_id", request.LobbyID}},
-		bson.D{{"$set", bson.D{{"players", lobby.Players}}}},
-	)
-	return err
-}
-
-func CreateLobby(request ConnectionJSONRequest, lobbyTable *mongo.Collection) error {
-	log.Println("Creating lobby", request.LobbyID)
-
-	lobby, err := loadDefaultLobby()
-	if err != nil {
-		return err
-	}
-	lobby.ID = request.PlayerID
-	lobby.Players = append(lobby.Players, request.PlayerID)
-
-	_, err = lobbyTable.InsertOne(context.TODO(), lobby)
-	if err != nil {
-		return err
-	}
-	return nil
 }
