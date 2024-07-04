c21edfd1dbf752bdc01565f3988594c022e32cce
NikitDudinov <dudinovnik@yandex.ru>
2024-07-04 17:53:08 +0300

    сделал отображение игрока в лобби

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,22 +1,22 @@
 let ws = new WebSocket("ws://127.0.0.1:8080/lobby");
 let playerId = parseInt(document.querySelector('.player_id').value);
-
+let listPlayers = document.querySelector('.list-players');
 
 
 ws.onmessage = (msg) => {
+    
     console.log(JSON.parse(msg.data));
     let data = JSON.parse(msg.data);
     for (let i = 0; i < data.players.length ; i++) {
         let id = data.players[i];
         foundUser(id).then(() => {});
-
         async function foundUser(id) {
             let response = await fetch('/api/player/' + id + '/user', {
                 method: 'GET'
             });
             let user = await response.json();
             let login = user.login;
-            console.log(login);
+            listPlayers.appendChild(createPlayer(login))
         }
     }
     
