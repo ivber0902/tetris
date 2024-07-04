7130bc3d0f79140eb9f49c1adffdcea0db17c474
Dima Belyaev <dima.dmitry663@gmail.com>
2024-07-04 13:39:36 +0300

    start socket

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,7 +2,6 @@ const connection = new WebSocket("ws://localhost:9000");
 let a = 1;
 // если соединение успешно установлено
 connection.onopen = (event) => {  
-
     console.log("Hello METANIT.COM");
     
 };
@@ -19,9 +18,23 @@ connection.onmessage = (event) =>{
     console.log("Server response:", event.data);
 };
 connection.onmessage = (event) =>{ 
-    document.querySelector('.players__count').innerHTML = event.data;
-    let count = event.data;
-    connection.send("her");
+    let count = JSON.parse(event.data);
+    console.log("her");
+    console.log(count);
+    id = count.players[0];
+    console.log(id);
+
+    async function sendResult(id) {
+        let response = await fetch('/api/found', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: id
+            })
+        });
+        console.log(response);
+    }
+
+    sendResult(id).then(() => {});
 };
 
 
