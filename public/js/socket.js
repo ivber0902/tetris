e0a33335d6bb712cd4d05626ea344cae69ea44d0
Dima Belyaev <dima.dmitry663@gmail.com>
2024-07-05 09:11:24 +0300

    show lobby link

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,12 +1,24 @@
-let ws = new WebSocket("ws://127.0.0.1:8080/lobby");
+const urlParams = new URLSearchParams(window.location.search);
+const lobbyId = urlParams.get('lobby');
+
+// Создайте WebSocket-соединение с параметром lobby
+let wsUrl = "ws://127.0.0.1:8080/lobby";
+if (lobbyId) {
+    wsUrl += "?lobby=" + lobbyId;
+}
+let ws = new WebSocket(wsUrl);
 let playerId = parseInt(document.querySelector('.player_id').value);
 let listPlayers = document.querySelector('.list-players');
+let lobbyLink = document.querySelector('.lobby-link').innerHTML;
 
 
-ws.onmessage = (msg) => {
+ws.onmessage = (msg) => {  
     
     console.log(JSON.parse(msg.data));
     let data = JSON.parse(msg.data);
+    if (data.id) {
+        document.querySelector('.lobby-link').innerHTML = lobbyLink + '?lobby=' + data.id;
+    }
     for (let i = 0; i < data.players.length ; i++) {
         let id = data.players[i];
         foundUser(id).then(() => {});
@@ -19,6 +31,7 @@ ws.onmessage = (msg) => {
             listPlayers.appendChild(createPlayer(login))
         }
     }
+    
 }
 
 ws.onopen = () => {
@@ -28,5 +41,3 @@ ws.onopen = () => {
             "player_id": playerId
     }}));
 };
-
-
