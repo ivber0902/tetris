fd3b6728a6007e436c9a3f4dfb3309e5e4025dfc
Dima Belyaev <dima.dmitry663@gmail.com>
2024-07-11 09:40:40 +0300

    получение конфига мультиплеера

--- public/js/multiplayer-settings.js
+++ public/js/multiplayer-settings.js
@@ -5,92 +5,99 @@ let playerField = document.querySelector('.wrapper-main-field');
 let otherPlayers =  document.querySelectorAll('.player__username')
 let player;
 let otherField;
+let ui;
 const canvas = document.getElementById('game');
 const field = canvas.getContext('2d');
 
-let wsUrl = "ws://" + host + ":8080/lobby";
+let wsUrl = "ws://" + host + ":8080/game";
 if (lobbyId) {
     wsUrl += "?lobby=" + lobbyId;
 }
 
-let array = {
-    buffer: 1,
-    blocks : [6, 2, 3, 4, 5]
-}
 
 let ws = new WebSocket(wsUrl);
 
 ws.onopen = () => {
     ws.send(JSON.stringify({
-        "type": "get",
+        "type": "config"
     }));
 }
 
 
 ws.onmessage = (msg) => {
     let data = JSON.parse(msg.data);
-    let players = data.players;
-    let i = 0;
-    console.log(array)
-    players.forEach((id)=>{
-        foundUser(id).then((player)=>{
-            if(id === parseInt(playerField.id)){
-                playerField.querySelector('.player_name').textContent = player.login
-            }else{
-                otherPlayers[i].textContent = player.login
-                i++
-            }          
-        })
-
-    })
-    document.querySelector('.main').style.backgroundImage = `url(${data.settings.background})`
-    GAME.width = data.settings.play_field.width;
-    GAME.height = data.settings.play_field.height;
-    const ui = new UI(
-        34,
-        document.querySelector(".buffer__figure"),
-        document.querySelectorAll(".figure"),
-        GAME,
-        document.querySelector(".game__score"),
-        document.querySelector(".game__level"),
-    );
-    
-    ui.music = new Audio(data.settings.music)
-    player = new Player(ui, GAME.figuresQueueSize);  
-    player.lvl = data.settings.difficulty;
-    otherField = document.querySelectorAll('.other-field');
-    canvas.width = ui.field.width;
-    canvas.height = ui.field.height;
-    field.fillStyle = 'black';
-    field.fillRect(0, 0, ui.field.width, ui.field.height);
-    otherField.forEach((elem) => {
-        elem.width = ui.field.width;
-        elem.height = ui.field.height;
-        switch (ui.field.width / ui.blockSize) {
-            case 7:
-                elem.style.maxHeight = "480px";
-                document.querySelector('.palyers-list').style.paddingRight = '100px'
-                break
-            case 10:
-                elem.style.maxHeight = "400px";
-                break
-            case 15:
-                elem.style.maxHeight = "320px";
-                break
-        }
-    }) 
-    GAME.init(player, ui)
-    player.buffer = player.getFigure(array.buffer);
-    player.ui.buffer.src = player.buffer.image.src;
-    player.currentFigure = player.getFigure(array.blocks[0]);
-    player.currentFigure.x = player.getStartX(player.currentFigure);
-    player.nextFigures = [];
-    for (let i = 0; i < player.figuresQueueSize; i++) {
-        player.nextFigures[i] = player.getFigure(array.blocks[i]);
-        player.ui.viewNextFigures[i].src = player.nextFigures[i].image.src;
+    if (data.type === "config"){
+        console.log(data.config)
+        GAME.width = data.config.settings.play_field.width;
+        GAME.height = data.config.settings.play_field.height;
+        document.querySelector('.main').style.backgroundImage = `url(${data.config.settings.background})`
+        ui = new UI(
+            34,
+            document.querySelector(".buffer__figure"),
+            document.querySelectorAll(".figure"),
+            GAME,
+            document.querySelector(".game__score"),
+            document.querySelector(".game__level"),
+        );
+        ui.music = new Audio(data.config.settings.music)
+        player = new Player(ui, GAME.figuresQueueSize);  
+        player.lvl = data.config.settings.difficulty;
+        otherField = document.querySelectorAll('.other-field');
+        canvas.width = ui.field.width;
+        canvas.height = ui.field.height;
+        field.fillStyle = 'black';
+        field.fillRect(0, 0, ui.field.width, ui.field.height);
+        otherField.forEach((elem) => {
+            elem.width = ui.field.width;
+            elem.height = ui.field.height;
+            switch (ui.field.width / ui.blockSize) {
+                case 7:
+                    elem.style.maxHeight = "480px";
+                    document.querySelector('.palyers-list').style.paddingRight = '100px'
+                    break
+                case 10:
+                    elem.style.maxHeight = "400px";
+                    break
+                case 15:
+                    elem.style.maxHeight = "320px";
+                    break
+            }
+        }) 
+        GAME.init(player, ui)
+        ws.send(JSON.stringify({
+            "type": "all"
+        }));
     }
+    
+    if (data.type === "update"){
+        console.log(data)
+        // let players = data.state;
+        // let i = 0;
+        // players.forEach((id)=>{
+        //     foundUser(id).then((player)=>{
+        //         if(id === parseInt(playerField.id)){
+        //             playerField.querySelector('.player_name').textContent = player.login
+        //         }else{
+        //             otherPlayers[i].textContent = player.login
+        //             i++
+        //         }          
+        //     })
+        // })
+        player.buffer = player.getFigure(data.state.buffer);
+        player.ui.buffer.src = player.buffer.image.src;
+        player.currentFigure = player.getFigure(data.state.figures[0]);
+        player.currentFigure.x = player.getStartX(player.currentFigure);
+        player.nextFigures = [];
+        for (let i = 0; i < player.figuresQueueSize; i++) {
+            player.nextFigures[i] = player.getFigure(data.state.figures[i]);
+            player.ui.viewNextFigures[i].src = player.nextFigures[i].image.src;
+        }
+    }
+    
+    
+    
+    
     GAME.start(player, field, ui)
-    console.log(player.nextFigures)
 }
 
 async function foundUser(id)
