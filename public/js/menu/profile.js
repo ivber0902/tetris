1c60da64b1e99e2add419186b6da5fd0d27d5e82
NikitDudinov <dudinovnik@yandex.ru>
2024-07-22 17:21:14 +0300

    сделал достижения

--- public/js/menu/profile.js
+++ public/js/menu/profile.js
@@ -2,9 +2,9 @@ let button = document.querySelector('.back');
 let userImageInput = document.getElementById('image-input');
 let userImageProfile = document.querySelector('.user-avatar')
 
-button.addEventListener('click', ()=>{
-    window.history.back();
-})
+// button.addEventListener('click', ()=>{
+//     window.history.back();
+// })
 
 userImageInput.addEventListener('change', (event)=>{
     const file = event.target.files[0];
@@ -26,3 +26,41 @@ async function ChangeImage(image) {
     });
     console.log(response)
 }
+
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('.achievement'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
--- public/js/settings.js
+++ public/js/settings.js
@@ -328,6 +328,206 @@ const langArr = {
         "ru": "количество линий",
         "en": "number of lines",
     },
+    "achievement__title.path-legend": {
+        "ru": "путь легенды",
+        "en": "The Path of the Legend",
+    },
+    "item__title.path-legend.easy": {
+        "ru": "Сыграй 10 игр",
+        "en": "Play 10 games",
+    },
+    "item__title.path-legend.medium": {
+        "ru": "Сыграй 50 игр",
+        "en": "Play 50 games",
+    },
+    "item__title.path-legend.hard": {
+        "ru": "Сыграй 100 игр",
+        "en": "Play 100 games",
+    },
+    "item__subtitle.path-legend.easy": {
+        "ru": "Ты только на старте своего пути! Пройдя 10 игр, ты сделал первые шаги к новой эре в своем игровом путешествии.",
+        "en": "You're just at the start of your journey! After completing 10 games, you have taken the first steps towards a new era in your gaming journey.",
+    },
+    "item__subtitle.path-legend.medium": {
+        "ru": "Твоя настойчивость приносит плоды! С 50 сыгранными играми ты уже формируешь свою игровую судьбу.",
+        "en": "Your perseverance is bearing fruit! With 50 games played, you are already shaping your gaming destiny.",
+    },
+    "item__subtitle.path-legend.hard": {
+        "ru": "Поздравляем, легенда! Достигнув 100 игр, ты входишь в ряды мастеров, о которых шепчутся по всему игровому миру.",
+        "en": "Congratulations, legend! Having reached 100 games, you join the ranks of the masters, who are whispered about all over the gaming world.",
+    },
+    "achievement__title.legend-tetris": {
+        "ru": "легенда тетриса",
+        "en": "The legend of Tetris",
+    },
+    "item__title.legend-tetris.easy": {
+        "ru": "Набери за игру 10000 очков",
+        "en": "Score 10,000 points per game",
+    },
+    "item__title.legend-tetris.medium": {
+        "ru": "Набери за игру 10000 очков",
+        "en": "Score 50,000 points per game",
+    },
+    "item__title.legend-tetris.hard": {
+        "ru": "Набери за игру 10000 очков",
+        "en": "Score 100,000 points per game",
+    },
+    "item__subtitle.legend-tetris.easy": {
+        "ru": "Ты сделал первый шаг к бессмертию! 10000 очков — это начало твоей легенды!",
+        "en": "You have taken the first step towards immortality! 10,000 points is the beginning of your legend!",
+    },
+    "item__subtitle.legend-tetris.medium": {
+        "ru": "Ты на пути к небесам! 50000 очков делают тебя не только мастером, но и легендой!",
+        "en": "You're on your way to heaven! 50,000 points make you not only a master, but also a legend!",
+    },
+    "item__subtitle.legend-tetris.hard": {
+        "ru": "Ты достиг божественных высот! 100000 очков — это финальный аккорд, который подтверждает твое мастерство!",
+        "en": "You have reached divine heights! 100,000 points is the final chord that confirms your skill!",
+    },
+    "achievement__title.blitz": {
+        "ru": "Блиц-мастер",
+        "en": "Blitz Master",
+    },
+    "item__title.blitz.easy": {
+        "ru": `Набери в режиме "Блиц" 4000 очков`,
+        "en": `Score 4000 points in the "Blitz" mode`,
+    },
+    "item__title.blitz.medium": {
+        "ru": `Набери в режиме "Блиц" 8000 очков`,
+        "en": `Score 8000 points in the "Blitz" mode`,
+    },
+    "item__title.blitz.hard": {
+        "ru": `Набери в режиме "Блиц" 12000 очков`,
+        "en": `Score 12000 points in the "Blitz" mode`,
+    },
+    "item__subtitle.blitz.easy": {
+        "ru": `Твоя победная серия начинается! 4000 очков — это первый приз, свидетельствующий о твоем таланте.`,
+        "en": `Your winning streak begins! 4000 points is the first prize, which testifies to your talent.`,
+    },
+    "item__subtitle.blitz.medium": {
+        "ru": `Ты бьешь рекорды! 8000 очков — это заметный шаг к титулу мастера блица.`,
+        "en": `You're breaking records! 8000 points is a significant step towards the title of blitz master.`,
+    },
+    "item__subtitle.blitz.hard": {
+        "ru": `Ты достиг небывалых высот! 12000 очков — это настоящее чудо, подтверждающее твою гениальность в игре.`,
+        "en": `You have reached unprecedented heights! 12,000 points is a real miracle, confirming your genius in the game.`,
+    },
+    "achievement__title.forty-line": {
+        "ru": `40 линий взяты`,
+        "en": `40 lines are taken`,
+    },
+    "item__title.forty-line.easy": {
+        "ru": `Собери 40 линий за 8 минут`,
+        "en": `Collect 40 lines in 8 minutes`,
+    },
+    "item__title.forty-line.medium": {
+        "ru": `Собери 40 линий за 5 минут`,
+        "en": `Collect 40 lines in 5 minutes`,
+    },
+    "item__title.forty-line.hard": {
+        "ru": `Собери 40 линий за 3 минут`,
+        "en": `Collect 40 lines in 3 minutes`,
+    },
+    "item__subtitle.forty-line.easy": {
+        "ru": `Неизвестные скорости! Завершив 40 линий за 8 минут, ты начал свой путь к скоростным вершинам.`,
+        "en": `Unknown speeds! Having completed 40 lines in 8 minutes, you have started your journey to the high-speed peaks.`,
+    },
+    "item__subtitle.forty-line.medium": {
+        "ru": `Смелый преодолитель времени! Зафиксировав 5 минут на 40 линий, ты демонстрируешь настоящую скорость.`,
+        "en": `The brave leader of time! By fixing 5 minutes on 40 lines, you demonstrate real speed.`,
+    },
+    "item__subtitle.forty-line.hard": {
+        "ru": `Ты стал мастером скорости! 3 минуты на 40 линий — это живое искусство быстроты и точности.`,
+        "en": `You've become a speed master! 3 minutes on 40 lines is a living art of speed and precision.`,
+    },
+    "achievement__title.royal": {
+        "ru": `Королевский дуэт`,
+        "en": `The Royal Duo`
+    },
+    "item__title royal.easy": {
+        "ru": "Собери 100 линий",
+        "en": "Collect 100 lines"
+    },
+    "item__title royal.medium": {
+        "ru": "Собери 500 линий",
+        "en": "Collect 500 lines"
+    },
+    "item__title royal.hard": {
+        "ru": "Собери 1000 линий",
+        "en": "Collect 1000 lines"
+    },
+    "item__subtitle.royal.easy": {
+        "ru": "Сила в единстве! Ваша команда собрала 100 линий, и ты уже на пути к настоящей гармонии!",
+        "en": "Strength is in unity! Your team has collected 100 lines, and you are already on the way to real harmony!"
+    },
+    "item__subtitle.royal.medium": {
+        "ru": "Ты и твой партнер — истинные чемпионы! 500 линий — это результат вашего единства и слаженности.",
+        "en": "You and your partner are true champions! 500 lines is the result of your unity and coherence."
+    },
+    "item__subtitle.royal.hard": {
+        "ru": "Ваше имя будет увековечено! 1000 линий — это легендарное достижение, которое заставит говорить о вас!",
+        "en": "Your name will be immortalized! 1000 lines is a legendary achievement that will make people talk about you!"
+    },
+    "achievement__title.multy": {
+        "ru": "Непобедимый мультиплеер",
+        "en": "Invincible multiplayer"
+    },
+    "item__title multy.easy": {
+        "ru": "Победи в 3 онлайн играх подряд",
+        "en": "Wins in 3 online games in a row"
+    },
+    "item__title multy.medium": {
+        "ru": "Победи в 5 онлайн играх подряд",
+        "en": "Wins in 5 online games in a row"
+    },
+    "item__title multy.medium": {
+        "ru": "Победи в 10 онлайн играх подряд",
+        "en": "Wins in 10 online games in a row"
+    },
+    "item__subtitle.multy.easy": {
+        "ru": "Победи в 10 онлайн играх подряд",
+        "en": "Wins in 10 online games in a row"
+    },
+    "item__subtitle.multy.easy": {
+        "ru": "Ты заручился поддержкой удачи! Три победы подряд — это всего лишь начало великого пути!",
+        "en": "You've enlisted the help of luck! Three wins in a row is just the beginning of a great journey!"
+    },
+    "item__subtitle.multy.medium": {
+        "ru": "Победи в 5 онлайн играх подряд",
+        "en": "Wins in 5 online games in a row"
+    },
+    "item__subtitle.multy.hard": {
+        "ru": "Победи 10 соперников подряд",
+        "en": "Defeat 10 opponents in a row"
+    },
+    "achievement__title.ai": {
+        "ru": "Битва против ИИ",
+        "en": "The Battle against AI"
+    },
+    "item__title.ai.easy": {
+        "ru": "Победи ИИ 3 раза",
+        "en": "Defeat the AI 3 times"
+    },
+    "item__title.ai.easy": {
+        "ru": "Победи ИИ 5 раз",
+        "en": "Defeat the AI 5 times"
+    },
+    "item__title.ai.easy": {
+        "ru": "Победи ИИ 10 раз",
+        "en": "Defeat the AI 10 times"
+    },
+    "item__subtitle.ai.easy": {
+        "ru": "Твой первый осколок успеха! Победив 3 искусственных противника, ты показываешь свои шансы на победу!",
+        "en": "Your first piece of success! By defeating 3 artificial opponents, you show your chances of winning!"
+    },
+    "item__subtitle.ai.medium": {
+        "ru": "Ты наращиваешь дух и уверенность! 5 побед подряд показывают, что ты готов к большему!",
+        "en": "You build up your spirit and confidence! 5 wins in a row show that you are ready for more!"
+    },
+    "item__subtitle.ai.hard": {
+        "ru": "Ты поразил систему! 10 побед подряд — ты стал истинным мастером сражений!",
+        "en": "You've hit the system! 10 wins in a row — you have become a true master of battles!"
+    },
     
 }
 if((localStorage.lang === undefined) || (!localStorage.lang)){
